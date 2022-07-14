3c7365c77b5b84cda1b9c2dfb0d61c55d119a77e
JeffersonRolino <jefferson.rolino@gmail.com>
2022-07-14 09:09:53 -0300

    Estrutura do Spring Boot criada com o Spring Initializr

---
+++ src/test/java/com/github/jeffersonrolino/Compass/uol_Avaliacao_Sprint4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jeffersonrolino.Compass.uol_Avaliacao_Sprint4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
